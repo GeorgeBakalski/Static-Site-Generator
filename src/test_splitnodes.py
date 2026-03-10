1c332a9b3106a025a756a163481ba46a1bb9992e
Georgios Bakalski <george1216.gb@gmail.com>
2026-03-11 00:25:26 +0200

    text to textnodes + tests

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode,TextType
 
 class TestTextNode(unittest.TestCase):
@@ -152,5 +152,41 @@ class TestTextNode(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnodes_all(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        split_node = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            split_node,
+        )
+
+    def test_text_to_textnodes_just_text(self):
+        text = "just text"
+        split_node = text_to_textnodes(text)
+        self.assertListEqual([TextNode("just text", TextType.TEXT)], split_node)
+
+    def test_text_to_textnodes_just_bold_text(self):
+        text = "just **bold** text"
+        split_node = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("just ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" text", TextType.TEXT),
+            ],
+            split_node,
         )
     
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from htmlnode import LeafNode
 
+
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
