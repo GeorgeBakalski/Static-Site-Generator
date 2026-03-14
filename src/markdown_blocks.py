fbd9a8cf4c40d16489b06756557ec874e4351c4c
Georgios Bakalski <george1216.gb@gmail.com>
2026-03-14 14:16:38 +0200

    markdown to html node + tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from htmlnode import HTMLNode, ParentNode
+from textnode import text_node_to_html_node, TextNode, TextType
+from inline_markdown import text_to_textnodes
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -51,3 +54,104 @@ def markdown_to_blocks(markdown):
             return_list.append(list_of_blocks[i])
     return return_list
             
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    all_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.CODE:  
+            all_nodes.append(ParentNode("pre", strip_code(block)))
+
+        elif block_type == BlockType.UNORDERED_LIST:
+            all_nodes.append(ParentNode(block_type_to_tag(block_type, block), strip_unordered_list(block)))
+
+        elif block_type == BlockType.ORDERED_LIST:
+            all_nodes.append(ParentNode(block_type_to_tag(block_type, block), strip_ordered_list(block)))
+
+        elif block_type == BlockType.QUOTE:
+            all_nodes.append(ParentNode(block_type_to_tag(block_type, block), text_to_children(strip_quote(block))))
+
+        elif block_type == BlockType.HEADING:
+            all_nodes.append(ParentNode(block_type_to_tag(block_type, block), text_to_children(strip_heading(block))))
+
+        else:
+            all_nodes.append(ParentNode(block_type_to_tag(block_type, block), text_to_children(block.replace("\n", " "))))
+    return ParentNode("div", all_nodes)
+        
+def strip_heading(block):
+    if block.startswith("# "):
+        return block[2:]
+    if block.startswith("## "):
+        return block[3:]
+    if block.startswith("### "):
+        return block[4:]
+    if block.startswith("#### "):
+        return block[5:]
+    if block.startswith("##### "):
+        return block[6:]
+    if block.startswith("###### "):
+        return block[7:]
+    return block
+
+def strip_quote(block):
+    lines = block.split("\n")
+    children = []
+    for line in lines:
+        clean = line[2:].rstrip()
+        children.append(clean)
+    return "\n".join(children)
+
+def strip_unordered_list(block):
+    lines = block.split("\n")
+    children = []
+    for line in lines:
+        clean = line[2:].rstrip()
+        children.append(ParentNode("li", text_to_children(clean)))
+    return children
+
+def strip_ordered_list(block):
+    lines = block.split(f"\n")
+    children = []
+    for line in lines:
+        content = line.split(". ", 1)[1]
+        children.append(ParentNode("li", text_to_children(content)))
+    return children
+
+def strip_code(block):
+    textnode = TextNode(block[4:-3], TextType.TEXT)
+    return  [ParentNode("code", [text_node_to_html_node(textnode)])]
+
+def text_to_children(block):
+    nodes = text_to_textnodes(block)
+    children = []
+    for node in nodes:
+        children.append(text_node_to_html_node(node))
+    return children
+        
+def block_type_to_tag(block_type, block):
+    if block_type == BlockType.PARAGRAPH:
+        return "p"
+    if block_type == BlockType.CODE:
+        return "code"
+    if block_type == BlockType.HEADING:
+        return heading_1_to_6(block)
+    if block_type == BlockType.QUOTE:
+        return "blockquote"
+    if block_type == BlockType.UNORDERED_LIST:
+        return "ul"
+    if block_type == BlockType.ORDERED_LIST:
+        return "ol"
+
+def heading_1_to_6(block):
+    if block.startswith("# "):
+        return "h1"
+    if block.startswith("## "):
+        return "h2"
+    if block.startswith("### "):
+        return "h3"
+    if block.startswith("#### "):
+        return "h4"
+    if block.startswith("##### "):
+        return "h5"
+    if block.startswith("###### "):
+        return "h6"
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -92,3 +92,127 @@ This is the same paragraph on a new line
             block_type,
             BlockType.PARAGRAPH
         )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+    )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_quoteblock(self):
+        md = """
+> This is **bolded** paragraph
+> text in a p
+> tag here
+> This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><blockquote>This is <b>bolded</b> paragraph\ntext in a p\ntag here\nThis is another paragraph with <i>italic</i> text and <code>code</code> here</blockquote></div>",
+    )
+        
+    def test_unordered_list_block(self):
+        md = """
+- This is **bolded** paragraph
+- text in a p
+- tag here
+- This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><ul><li>This is <b>bolded</b> paragraph</li><li>text in a p</li><li>tag here</li><li>This is another paragraph with <i>italic</i> text and <code>code</code> here</li></ul></div>",
+    )
+    
+    def test_ordered_list_block(self):
+        md = """
+1. This is **bolded** paragraph
+2. text in a p
+3. tag here
+4. This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><ol><li>This is <b>bolded</b> paragraph</li><li>text in a p</li><li>tag here</li><li>This is another paragraph with <i>italic</i> text and <code>code</code> here</li></ol></div>",
+    )
+        
+    def test_headingblock(self):
+        md = """
+# This is **bolded** paragraph
+
+#### text in a p
+
+##### tag here
+
+###### This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><h1>This is <b>bolded</b> paragraph</h1><h4>text in a p</h4><h5>tag here</h5><h6>This is another paragraph with <i>italic</i> text and <code>code</code> here</h6></div>",
+    )
+        
+    def test_all_blocks(self):
+        md = """
+# This is **bolded** heading
+
+#### text in another heading
+
+This is **bolded** paragraph
+
+> This is a 
+> quote with _italic_ text 
+> and `code` here
+
+```
+this is **just** _code_
+```
+
+- this is 
+- an unordered **list**
+
+1. this is
+2. an _ordered_ list
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><h1>This is <b>bolded</b> heading</h1><h4>text in another heading</h4><p>This is <b>bolded</b> paragraph</p><blockquote>This is a\nquote with <i>italic</i> text\nand <code>code</code> here</blockquote><pre><code>this is **just** _code_\n</code></pre><ul><li>this is</li><li>an unordered <b>list</b></li></ul><ol><li>this is</li><li>an <i>ordered</i> list</li></ol></div>",
+    )
