558649acdc1cb35fe70ae3a5a3bb65060ce2f733
Georgios Bakalski <george1216.gb@gmail.com>
2026-03-16 23:20:10 +0200

    github pages adjustments

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ from markdown_blocks import markdown_to_html_node, extract_title
 import os
 from pathlib import Path
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path,"r") as f:
         markdown = f.read()
@@ -12,7 +12,9 @@ def generate_page(from_path, template_path, dest_path):
     html_string = node.to_html()
     title = extract_title(markdown)
     template_with_title = template.replace( "{{ Title }}", title)
-    final_template = template_with_title.replace( "{{ Content }}", html_string)
+    content_template = template_with_title.replace( "{{ Content }}", html_string)
+    href_template = content_template.replace( 'href="/', f'href="{basepath}')
+    final_template = href_template.replace( 'src="/', f'src="{basepath}')
     
     dir = os.path.dirname(dest_path)
     if dir:
@@ -21,11 +23,11 @@ def generate_page(from_path, template_path, dest_path):
         page.write(final_template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         entry_path = (os.path.join(dir_path_content, entry))
         if os.path.isfile(entry_path):
             dest_path = Path(os.path.join(dest_dir_path, entry))
-            generate_page(entry_path,template_path, dest_path.with_suffix(".html"))
+            generate_page(entry_path,template_path, dest_path.with_suffix(".html"), basepath)
         else:
-            generate_pages_recursive(entry_path, template_path, os.path.join(dest_dir_path, entry))
+            generate_pages_recursive(entry_path, template_path, os.path.join(dest_dir_path, entry), basepath)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from textnode import TextType, TextNode
-import os, shutil
+import os, shutil, sys
 from copystatic import copy_files_recursive
 from generate_page import generate_page, generate_pages_recursive
+ 
 
 def main():
-    if os.path.exists("./public"):
-        shutil.rmtree("./public")
-    copy_files_recursive("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+    copy_files_recursive("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 main()
